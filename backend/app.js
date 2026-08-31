7f4c69915e7b7f7bf1f02a5dc866319437ee0523
Rishabh Shankar Prasad <rishabhsp125@gmail.com>
2026-08-31 23:18:29 +0530

    Add credentials: true in line 26 of app.js

--- backend/app.js
+++ backend/app.js
@@ -13,16 +13,20 @@ import authRoutes from "./routes/auth.route.js";
 import membershipPlanRoutes from "./routes/membershipPlan.routes.js";
 import trainerRoutes from "./routes/trainer.routes.js";
 import classScheduleRoutes from "./routes/classSchedule.routes.js";
-import membershipRegistrationRoutes from "./routes/membershipRegistration.routes.js"
+import membershipRegistrationRoutes from "./routes/membershipRegistration.routes.js";
 import enquiryRoutes from "./routes/enquiry.routes.js";
 
 import errorMiddleware from "./middleware/errorMiddleware.js";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL,
-}));
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -30,13 +34,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/membership-plans", membershipPlanRoutes);
 app.use("/api/trainers", trainerRoutes);
 app.use("/api/class-schedules", classScheduleRoutes);
-app.use("/api/membership-registrations", membershipRegistrationRoutes)
-app.use("/api/enquiries", enquiryRoutes)
+app.use("/api/membership-registrations", membershipRegistrationRoutes);
+app.use("/api/enquiries", enquiryRoutes);
 
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`App is listening on ${PORT}`);
-})
+  console.log(`App is listening on ${PORT}`);
+});
